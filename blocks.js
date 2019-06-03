cc3b0f8c0d15c2acf55abedebfbbf6fb1b538848
Ewa Potorecka <ewa.potorecka@gmail.com>
2019-06-03 18:44:17 +0200

    wszystko dziala pieknie smiga

--- blocks.js
+++ blocks.js
@@ -1,3 +1,7 @@
+const tileSize = 50;
+
+main();
+
 function main() {
 	const scene = {
 		board: {
@@ -10,32 +14,80 @@ function main() {
 				position: { x: 3, y: 4 },
 				height: 3,
 				width: 1
+			},
+			{
+				position: { x: 1, y: 2 },
+				height: 3,
+				width: 1
+			},
+			{
+				position: { x: 6, y: 6 },
+				height: 2,
+				width: 2
 			}
 		],
-		exit: { x: 0, y: -1 },
+		exit: { x: 0, y: 0 },
 		playerPosition: { x: 9, y: 9 }
 	};
+	const canvas = document.getElementById( 'blocksBoard' );
+	const ctx = canvas.getContext( '2d' );
+	canvas.height = scene.board.height * tileSize;
+	canvas.width = scene.board.width * tileSize;
+
+	renderBoard( scene, ctx );
 
 	document.addEventListener( 'keyup', event => {
 		if ( event.key === 'ArrowUp' ) {
 			movePlayer( scene, { x: 0, y: -1 } );
+			renderBoard( scene, ctx );
 		} else if ( event.key === 'ArrowDown' ) {
 			movePlayer( scene, { x: 0, y: 1 } );
+			renderBoard( scene, ctx );
 		} else if ( event.key === 'ArrowLeft' ) {
 			movePlayer( scene, { x: -1, y: 0 } );
+			renderBoard( scene, ctx );
 		} else if ( event.key === 'ArrowRight' ) {
 			movePlayer( scene, { x: 1, y: 0 } );
+			renderBoard( scene, ctx );
 		}
 	} );
 }
 
-function renderBoard() {
-	drawBoard();
-	drawBlocks();
-	drawPlayer();
-	drawExit();
+function renderBoard( scene, ctx ) {
+	clearCanvas( ctx );
+	drawBoard( scene, ctx );
+	drawBlocks( scene, ctx );
+	drawPlayer( scene, ctx );
+	drawExit( scene, ctx );
 }
 
+function drawBoard( scene, ctx ) {
+	const board = scene.board;
+
+	ctx.strokeRect( board.position.x * tileSize, board.position.y * tileSize, board.width * tileSize, board.height * tileSize );
+}
+
+function drawBlocks( scene, ctx ) {
+	for ( const block of scene.blocks ) {
+		ctx.fillRect( block.position.x * tileSize, block.position.y * tileSize, block.width * tileSize, block.height * tileSize );
+	}
+}
+
+function drawPlayer( scene, ctx ) {
+	const player = scene.playerPosition;
+	ctx.beginPath();
+	ctx.arc( player.x * tileSize + tileSize / 2, player.y * tileSize + tileSize / 2, tileSize / 2, 0, 2 * Math.PI );
+	ctx.fill();
+}
+
+function drawExit( scene, ctx ) {
+	const exit = scene.exit;
+	ctx.strokeRect( exit.x * tileSize, exit.y * tileSize, tileSize, tileSize );
+}
+
+function clearCanvas( ctx ) {
+	ctx.clearRect( 0, 0, ctx.canvas.width, ctx.canvas.height );
+}
 function movePlayer( scene, moveVector ) {
 	const newPosition = {
 		x: scene.playerPosition.x + moveVector.x,
@@ -80,7 +132,10 @@ function isEmptyAt( scene, position ) {
 }
 
 function canBlockBeMoved( scene, block, moveVector ) {
-	const filteredBlocks = scene.blocks.filter( blockInArr => block !== blockInArr );
+	const sceneWithoutMovedBlock = {
+		...scene,
+		blocks: scene.blocks.filter( blockInArr => block !== blockInArr )
+	};
 	const blockPartialPositions = getBlockPartialPositions( block );
 
 	for ( const partialPosition of blockPartialPositions ) {
@@ -93,7 +148,7 @@ function canBlockBeMoved( scene, block, moveVector ) {
 			return false;
 		}
 
-		if ( !isEmptyAt( filteredBlocks, newPosition ) ) {
+		if ( !isEmptyAt( sceneWithoutMovedBlock, newPosition ) ) {
 			return false;
 		}
 	}
